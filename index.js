fd3a142fc609d3be8e376d07c3c574811ca3379d
Julissa Ramirez <jmrami14@asu.edu>
2022-12-31 00:21:13 -0700

    Simple JS Functiont

---
+++ index.js
@@ -0,0 +1,8 @@
+function info(Name, Height, Country) {
+    alert('Name: Jackie Smith');
+    alert('Height: 175.72cm');
+    alert('Country: United States');
+ 
+ }
+ 
+ info('Jackie Smith', '175.72cm', 'United States')
